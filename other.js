39f37eead27de468e60db6a4b92cfed6d512d05d
Randall Burgess <randallallenburgess@gmail.com>
2020-10-06 15:20:16 -0700

    added stripe bar, added new buttons and reset functionality via new colors button

--- other.js
+++ other.js
@@ -1,12 +1,14 @@
 const squares = document.querySelectorAll(".square");
 const display = document.querySelector("#color-display");
 const message = document.querySelector("#message");
+const announce = document.querySelector("#announce");
+const resetButton = document.querySelector("#reset");
 
 const randomColor = () => {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + b + ", " + b + ")";
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 const changeColors = (color) => {
@@ -35,10 +37,18 @@ const generateRandomColors = (num) => {
 
 
 let colors = generateRandomColors(6);
-const winningColor = pickColor();
+let winningColor = pickColor();
 //show color winning color
 display.textContent = winningColor;
 
+resetButton.addEventListener("click", () => {
+    colors = generateRandomColors(6);
+    winningColor = pickColor();
+    display.textContent = winningColor;
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+})
 
 
 for(let i = 0; i < squares.length; i++){
@@ -47,12 +57,13 @@ for(let i = 0; i < squares.length; i++){
     //add click event to squares
     squares[i].addEventListener("click", function(){
         //grab color of clicked square
-        var clickedColor = this.style.backgroundColor;
+        let clickedColor = this.style.backgroundColor;
         //compare clicked color to winning
         console.log(clickedColor)
         if(clickedColor === winningColor){
             message.textContent = "YOU WIN";
             changeColors(clickedColor);
+            announce.style.backgroundColor = clickedColor;
         }else {
             squares[i].style.backgroundColor = "#232323";
             message.textContent = "Try Again";
